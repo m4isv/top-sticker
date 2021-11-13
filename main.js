c477ce4df233198afb06577a9e6a573f9ee95254
msnpy <53246704+msnpy@users.noreply.github.com>
2021-11-13 11:01:52 -0300

    versao1

---
+++ main.js
@@ -0,0 +1,37 @@
+//1
+setInterval(function(){
+let photo = Math.floor(Math.random() * 51)
+let img = document.querySelector("img").src = `_1/${photo}.png`
+	
+		
+}, 3000);
+
+
+//2
+setInterval(function(){
+let photo2 = Math.floor(Math.random() * 86)
+let img = document.querySelector("#img2").src = `_2/${photo2}.png`
+	
+		
+}, 3000);
+
+
+//3
+setInterval(function(){
+let photo3 = Math.floor(Math.random() * 30)
+let img = document.querySelector("#img3").src = `_3/${photo3}.png`
+	
+		
+}, 3000);
+
+
+
+
+//4
+setInterval(function(){
+let photo4 = Math.floor(Math.random() * 45)
+let img = document.querySelector("#img4").src = `_4/${photo4}.png`
+	
+		
+}, 3000);
+
